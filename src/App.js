75ac6df812d81be0c7462a2ccf4398d96adb8b2c
하송희 <hshee1122@naver.com>
2022-06-24 23:56:18 +0900

    todoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Template from "./components/Template";
+import TodoList from "./components/TodoList";
+import { MdAddCircle } from "react-icons/md";
+import TodoInsert from "./components/TodoInsert";
 
-function App() {
+let nextId = 4;
+
+const App = () => {
+  const [insertToggle, setInsertToggle] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "피자랑 치킨 먹기",
+      explain: "점심 저녁 나눠서",
+      checked: true
+    },
+    {
+      id: 2,
+      text: "리액트 공부하기",
+      explain: "투두리스트 만들기",
+      checked: true
+    },
+    {
+      id: 3,
+      text: "사내맞선 정주행",
+      explain: "넷플릭스로 보기",
+      checked: false
+    }
+  ]);
+
+  const onInsertToggle = () => {
+    setInsertToggle(prev => !prev);
+  };
+
+  const onInsertTodo = ({text, explain}) => {
+    if (explain === "") {
+      return alert("빈 칸을 채워주세요 !");
+    } 
+    if (text === "") {
+      return alert("빈 칸을 채워주세요 !");
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        explain,
+        checked: false
+      };
+      setTodos(todos => todos.concat(todo));
+      nextId++;
+    }
+  };
+
   return (
-    <div className='App'>
-      <h1>Sequence Makes Difference</h1>
-      <h1>과제 화이팅입니다.</h1>
-      <h2>제출 기한은 6월 23일 23:59</h2>
+  <Template todoLength={todos.length}>
+    <TodoList todos={todos} />
+    <div className="add-todo-button" onClick={onInsertToggle} >
+      <MdAddCircle />
     </div>
+    {insertToggle && (
+        <TodoInsert
+          //selectedTodo={selectedTodo}
+          onInsertToggle={onInsertToggle}
+          onInsertTodo={onInsertTodo}
+          //onRemove={onRemove}
+          //onUpdate={onUpdate}
+        />
+      )}
+    </Template>
   );
-}
+}; 
 
 export default App;
---
+++ src/components/TodoInsert.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { MdAddCircle } from "react-icons/md";
+import "./TodoInsert.css";
+
+
+const TodoInsert = ({onInsertToggle, onInsertTodo}) => {
+  const [value, setValue] = useState("");
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    onInsertTodo(value);
+    setValue("");
+    onInsertToggle();
+  };
+
+    return (
+      <div>
+        <div className="background" onClick={onInsertToggle}></div>
+        <form onSubmit={onSubmit}>
+          <input placeholder="할 일을 입력하세요" value={value} onChange={onChange}></input>
+          <input placeholder="설명을 입력하세요" value={value} onChange={onChange}></input>
+          <button type="submit">
+            <MdAddCircle />
+          </button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default TodoInsert;
